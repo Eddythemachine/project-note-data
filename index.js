8f7565270da5ca5c3200e8fae9aa530432c7d796
Onayifeke Edison Oghenerunor <tkeddy5@gmail.com>
2023-06-08 12:49:42 +0100

    Bug fixed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defauklts();
+const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
